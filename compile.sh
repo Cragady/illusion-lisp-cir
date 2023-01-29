389badcdb4b85c8bb5d7696b2b2cfc62130bd6c6
Cragady <pinkdotdreamer@gmail.com>
2023-01-29 04:05:05 -0700

    feat: automate windows aliased sbcl for exe

--- compile.sh
+++ compile.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-sbcl --eval '(pushnew (uiop:getcwd) ql:*local-project-directories*)' \
-    --eval '(ql:quickload :illusion-lisp-cir)' \
+shopt -s expand_aliases
+source ~/.bashrc
+bsbcl --eval '(ql:quickload :illusion-lisp-cir)' \
     --eval '(asdf:make :illusion-lisp-cir)' --eval '(quit)'
